687ff4a6fd68b50bffee03e74c91be242f112970
Ali Kia <ali.kia@stud.hcw.ac.at>
2026-05-07 01:04:39 +0200

    transition between pages in logIn and Sign Up

---
+++ KanUbake/client/LogIn-SinUp/js/transition-between-pages.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => document.body.style.opacity = '1');
+document.querySelectorAll('a').forEach(a => {
+  a.addEventListener('click', e => {
+    e.preventDefault();
+    const href = a.href;
+    document.body.style.opacity = '0';
+    setTimeout(() => location.href = href, 200);
+  });
+});
