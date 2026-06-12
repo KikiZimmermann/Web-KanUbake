bc229753d19cbc0002876d22b39a07d3f8279dbe
alena-roth <alena.roth2@gmail.com>
2026-06-12 12:03:51 +0200

    fixed order of nav border parameters to support all browsers

--- KanUbake/client/fixedLayout/header.js
+++ KanUbake/client/fixedLayout/header.js
@@ -5,7 +5,4 @@ fetch("/KanUbake/client/fixedLayout/header.html")
 
         document.getElementById("pageTitle").textContent =
             window.pageTitle || "KanUbake";
-
-        // initialize the auth button after the header HTML is in the DOM
-        if (typeof initAuthBtn === "function") initAuthBtn();
     });
